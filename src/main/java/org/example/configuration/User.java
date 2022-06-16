03e9fa2723a4ceb7a2cb6b0d74c2c1b47377ba5f
MagicValue <sanek1023jr12@yandex.by>
2022-06-16 19:08:21 +0300

    Initial commit

---
+++ src/main/java/org/example/configuration/User.java
@@ -0,0 +1,16 @@
+package org.example.configuration;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private Byte age;
+
+}
